35812a4bc9de0d1c8ca9b4a771f83fce8a06516c
Mike Goren and Tucker Mayo <mike@mortalwombat.net>
2015-03-03 12:03:31 -0800

    add sort by date functionality

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,7 +1,7 @@
 app.PostsController = Ember.ArrayController.extend({
   itemController: 'post',
 
-  sortProperties: ['text'],
+  sortProperties: ['date'],
   sortAscending: true,
 
   actions: {
@@ -10,9 +10,8 @@ app.PostsController = Ember.ArrayController.extend({
       posts.addObject({id: id, text: this.text, date: new Date});
       this.set("text", null);
     },
-    sortByText: function() {
-      this.set('sortProperties', ['text']);
-      debugger;
+    reverseSort: function() {
+      this.set('sortAscending', !this.get('sortAscending'));
     }
   }
 });
--- store.js
+++ store.js
@@ -1,18 +1,1 @@
-posts = [
-{
-  id: '1',
-  text: 'Before they sold out ugh crucifix skateboard, vinyl viral banjo. Church-key Marfa Tumblr farm-to-table, slow-carb iPhone semiotics gluten-free Odd Future disrupt.'
-},
-{
-  id: '2',
-  text: 'Wayfarers Kickstarter mustache umami meh church-key artisan, drinking vinegar lomo normcore. Narwhal actually American Apparel paleo irony vinyl.idae.'
-},
-{
-  id: '3',
-  text: 'Chillwave scenester jean shorts, you probably havent heard of them put a bird on it lomo polaroid forage retro wayfarers cronut.'
-},
-{
-  id: '4',
-  text: 'Fap post-ironic Etsy, small batch letterpress distillery fixie. Hashtag deep v salvia, migas twee pug Pinterest sustainable health goth artisan pop-up.'
-}
-];
+posts = [];
